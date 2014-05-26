24182694286d078b474b053a6c93904e21a7d206
Don Smith <locksmithdon@gmail.com>
2014-05-26 23:15:43 +1200

    Add angular & jquery and rough out some screens

---
+++ Trakl_Windows.Shared/lib/readme.txt
@@ -0,0 +1,4 @@
+The angular folder contains the entire build of 1.3.0 beta 10
+but only the files needed are included in the project. To include
+more functionality, include them in the project and they will be
+packed with the apps during a build/package.
--- Trakl_Windows.Shared/pages/home/home.js
+++ Trakl_Windows.Shared/pages/home/home.js
@@ -5,7 +5,25 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+
+            var settingsAppBarCommand = document.getElementById('settings');
+            settingsAppBarCommand.addEventListener('click', this.navigateToSettings, false);
+
+            var aboutAppBarCommand = document.getElementById('about');
+            aboutAppBarCommand.addEventListener('click', this.navigateToAbout, false);
+
+            WinJS.Binding.processAll(element, this);
+        },
+        trafficData: {
+            travelMinutes: '44',
+            adviceClass: 'should-go homepage fragment'
+        },
+        navigateToSettings: function () {
+            WinJS.Navigation.navigate('/pages/settings/settings.html');
+        },
+        navigateToAbout: function () {
+            WinJS.Navigation.navigate('/pages/about/about.html');
         }
     });
+
 })();
